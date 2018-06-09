5067533f13dc5ac52dfaf90962d6716116d821ec
jasmeetkaursaini <39778975+jasmeetkaursaini@users.noreply.github.com>
2018-06-09 16:53:19 -0400

    Add files via upload
    Day8

---
+++ Day8Game/src/day8game/jButton.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package day8game;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Jasmeet kaur
+ */
+class jButton {
+
+    void setIcon(ImageIcon X) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setEnebled(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
